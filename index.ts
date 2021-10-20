519eac81e47cb8621bc277daefdfaedb125e1d44
Vladislav Botvin <darkvlados@gmail.com>
2021-10-20 19:33:25 +0300

    cosmetic

--- index.ts
+++ index.ts
@@ -3,12 +3,7 @@ import { AsyncLocalStorage } from "async_hooks";
 const ls = new AsyncLocalStorage<{ deps: Map<unknown, unknown>, once: Map<unknown, unknown> }>();
 
 export const diDep = <T>(dep: T | string): T => {
-  const store = ls.getStore();
-
-  if (store == null) {
-    throw new Error('DI container not registered! Consider that you call "diInit" before');
-  }
-
+  const store = storeOrError();
   const userDep = (store.deps as Map<T | string, T>).get(dep);
 
   if (typeof dep === "string" && userDep == null) {
@@ -19,12 +14,7 @@ export const diDep = <T>(dep: T | string): T => {
 };
 
 export const diSet = <T>(dep: T, value: T extends string ? unknown : T) => {
-  const store = ls.getStore();
-
-  if (store == null) {
-    throw new Error('DI container not registered! Consider that you call "diInit" before');
-  }
-
+  const store = storeOrError();
   store.deps.set(dep, value);
 };
 
@@ -34,14 +24,19 @@ export const diInit = <T>(cb: () => T) => {
 
 export const diOnce = <T extends Function>(fn: T): T => {
   return (function(this: unknown, ...args: unknown[]) {
-    const store = ls.getStore();
-
-    if (store == null) {
-      throw new Error('DI container not registered! Consider that you call "diInit" before');
-    }
-
+    const store = storeOrError();
     return store.once.get(fn) ?? store.once.set(fn, fn.apply(this, args)).get(fn);
   }) as unknown as T
+}
+
+const storeOrError = () => {
+  const store = ls.getStore();
+
+  if (store == null) {
+    throw new Error('DI container not registered! Consider, that you call "diInit" before');
+  }
+
+  return store;
 }
 
 export const diExists = () => (ls.getStore() == null) === false
